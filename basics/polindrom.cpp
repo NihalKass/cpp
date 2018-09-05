453af543ea062811d25db6c41d14e6a8146b33c6
NihalKass <pappu.kr26@gmail.com>
2018-09-05 09:02:39 +0530

    polindrom for no.

---
+++ basics/polindrom.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int num,rev=0;
+    cin>>num;
+
+    int n=num;
+
+    while(num!=0)
+    {
+    rev=rev*10;
+    rev=rev +num%10;
+    num = num/10;
+    }
+    if(rev==n)
+        cout<<"polindrom";
+    else
+        cout<<"not polindrom";
+return 0;
+}
