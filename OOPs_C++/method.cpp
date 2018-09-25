755e493da46c08aba66c5c9b2e68560243a8e546
NihalKass <pappu.kr26@gmail.com>
2018-09-25 22:53:10 +0530

    oops-concept

---
+++ OOPs_C++/method.cpp
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class student{
+    public:
+    int id;
+    float salary;
+    string name;
+
+    void insert(int i,float s,string n){
+        id=i;
+        salary=s;
+        name=n;
+    }
+
+    void display(){
+        cout<<id<<" "<<name<<" "<<salary<<endl;
+    }
+};
+
+int main()
+{
+    student s1,s2; //created an object of student class
+    s1.insert(101,2000,"Nihal");
+    s2.insert(102,5000,"Ambuj");
+    s1.display();
+    s2.display();
+}
